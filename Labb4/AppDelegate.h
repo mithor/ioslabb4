f423f0e2370fc5d1df9cd95b3898ccd26cfa87eb
Mikael <it-hogskolan@Mikaels-Macbook.local>
2015-02-15 23:42:12 +0100

    Initial Commit

---
+++ Labb4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb4
+//
+//  Created by IT-Högskolan on 2015-02-15.
+//  Copyright (c) 2015 IT-Högskolan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Labb4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb4
+//
+//  Created by IT-Högskolan on 2015-02-15.
+//  Copyright (c) 2015 IT-Högskolan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
